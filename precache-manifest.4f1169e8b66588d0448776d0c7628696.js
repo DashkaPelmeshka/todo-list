45a4635b58854e1e99a684a3827150593f10595b
Daria <d-a-orlova@yandex.ru>
2020-01-21 00:07:48 +0300

    Updates

---
+++ precache-manifest.4f1169e8b66588d0448776d0c7628696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a19db59dd0706cd822175a913cfb0f",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "5832bfd6d6f8dc9602d5",
+    "url": "/todo-list/static/css/main.c1451d5b.chunk.css"
+  },
+  {
+    "revision": "ac394e22d87aeb179e1f",
+    "url": "/todo-list/static/js/2.872688d4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-list/static/js/2.872688d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5832bfd6d6f8dc9602d5",
+    "url": "/todo-list/static/js/main.6a00a15b.chunk.js"
+  },
+  {
+    "revision": "3c8bc7aa72310e7d0f4e",
+    "url": "/todo-list/static/js/runtime-main.901ffa1a.js"
+  }
+]);
